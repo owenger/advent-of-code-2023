610d4e60cc3c292ea5f3151eee4d363b55d0434b
Oliver <owenger@ethz.ch>
2024-01-06 09:38:12 +0100

    day12

---
+++ rust/day13/src/lib.rs
@@ -0,0 +1,17 @@
+use std::fs;
+use std::error::Error;
+use std::collections::HashMap;
+
+pub fn run_part_1(input_path: String) -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_adding_y_vectors() {
+        assert!(true);
+    }
+}
