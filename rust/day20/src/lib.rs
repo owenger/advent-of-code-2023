d0bf63950bcb005ff6676605614b33369f93d79c
Oliver <owenger@ethz.ch>
2024-01-24 07:34:58 +0100

    started with day20

--- rust/day20/src/lib.rs
+++ rust/day20/src/lib.rs
@@ -7,6 +7,24 @@ pub fn run_part_1(input_path: String) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+struct FlipFlop {
+    state: bool,
+}
+
+impl Pulsar for FlipFlop {
+    fn receive_pulse(&self, pulse: bool) -> Option<bool> {
+        if !pulse {
+            self.state = !self.state;
+            return Some(true);
+        }
+        None
+    }
+}
+
+trait Pulsar {
+    pub fn receive_pulse(&self, pulse: bool) -> Option<bool>;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
