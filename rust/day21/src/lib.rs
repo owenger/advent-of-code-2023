9cc12297fccdd5eaba1526fa706d2b9f720f223e
Oliver <owenger@ethz.ch>
2024-01-29 07:45:01 +0100

    started implementing dijkstra's

---
+++ rust/day21/src/lib.rs
@@ -0,0 +1,183 @@
+use std::fs;
+use std::error::Error;
+use std::collections::HashMap;
+use std::collections::BinaryHeap;
+use std::cmp;
+
+const FINDABLE_FIELDS: i32 = 7498;
+
+pub fn run_part_1(input_path: String) -> Result<(), Box<dyn Error>> {
+    let max_steps = 64;
+    let input = fs::read_to_string(input_path)?;
+    let mut grid = parse_input(input);
+    let start_row = grid.start_row;
+    let start_col = grid.start_col;
+    let total = walk_grid(&mut grid, start_row, start_col, max_steps);
+    println!("Total: {total}");
+    Ok(())
+}
+
+pub fn run_part_2(input_path: String) -> Result<(), Box<dyn Error>> {
+    let input_nr: i32 = 26501365;
+    let input = fs::read_to_string(input_path)?;
+    let mut grid = parse_input(input);
+    let start_row = grid.start_row;
+    let start_col = grid.start_col;
+    let max_steps = input_nr;//cmp::min(input_nr, grid.rows * grid.cols);
+    let total: i32 = 0;
+    let total = walk_grid(&mut grid, start_row, start_col, max_steps);
+    println!("Max steps to go: {}", grid.max_steps_to_go);
+    println!("Total: {total}");
+    Ok(())
+}
+
+fn dijkstras(grid: &mut Grid, max_steps: i32) -> i32 {
+    let mut visited: Vec<Cell> = Vec::new();
+    let mut heap = BinaryHeap::new();
+    heap.push(Cell{ row: grid.start_row, col: grid.start_col, cost: max_steps});
+
+
+    loop {
+        let cur_cell = heap.pop();
+        if visited.contains(cur_cell) {
+            continue;
+        }
+        visited.push(cur_cell.clone());
+    }
+    0
+}
+
+fn walk_grid(grid: &mut Grid, row: i32, col: i32, count: i32) -> i32 {
+    if row < 0 
+    || row >= grid.rows 
+    || col < 0 
+    || col >= grid.cols
+    || !grid.grid[row as usize][col as usize] {
+        return 0;
+    }
+    let mut total: i32 = 0;
+    if grid.hash_map.contains_key(&(row, col)) {
+        if grid.hash_map.get(&(row, col)).unwrap() >= &count {
+            return 0;
+        }
+        grid.hash_map.insert((row, col), count);
+    } else {
+        grid.hash_map.insert((row, col), count);
+        if count % 2 == 0 {
+            total = 1;
+        }
+    }
+    if count == 0 {
+        return 1;
+    }
+    total += walk_grid(grid, row + 1, col, count - 1);
+    if total == FINDABLE_FIELDS {
+        if count > grid.max_steps_to_go {
+            grid.max_steps_to_go = count;
+        }
+        return total;
+    }
+    total += walk_grid(grid, row - 1, col, count - 1);
+    if total == FINDABLE_FIELDS {
+        if count > grid.max_steps_to_go {
+            grid.max_steps_to_go = count;
+        }
+        return total;
+    }
+    total += walk_grid(grid, row, col + 1, count - 1);
+    if total == FINDABLE_FIELDS {
+        if count > grid.max_steps_to_go {
+            grid.max_steps_to_go = count;
+        }
+        return total;
+    }
+    total += walk_grid(grid, row, col - 1, count - 1);
+    if total == FINDABLE_FIELDS {
+        if count > grid.max_steps_to_go {
+            grid.max_steps_to_go = count;
+        }
+    }
+    total
+}
+
+fn parse_input(input: String) -> Grid {
+    let mut grid: Vec<Vec<bool>> = Vec::new();
+    let mut start: (i32, i32) = (0, 0);
+
+    for (row, line) in input.lines().enumerate() {
+        let mut grid_row: Vec<bool> = Vec::new();
+        for (col, ch) in line.chars().enumerate() {
+            match ch {
+                '.' => {
+                    grid_row.push(true);
+                }
+                '#' => grid_row.push(false),
+                'S' => {
+                    grid_row.push(true);
+                    start = (row as i32, col as i32);
+                }
+                _ => (),
+            }
+        }
+        grid.push(grid_row);
+    }
+    Grid::from(grid, start.0, start.1)
+}
+
+#[derive(Clone)]
+#[derive(Debug)]
+struct Grid {
+    grid: Vec<Vec<bool>>,
+    hash_map: HashMap<(i32, i32), i32>,
+    rows: i32,
+    cols: i32,
+    start_row: i32,
+    start_col: i32,
+    max_steps_to_go: i32,
+}
+
+impl Grid {
+    pub fn from(grid: Vec<Vec<bool>>, start_row: i32, start_col: i32) -> Grid {
+        let hash_map: HashMap<(i32, i32), i32> = HashMap::new();
+        Grid {
+            rows: grid.len() as i32,
+            cols: grid.first().map_or(0, Vec::len) as i32,
+            grid: grid,
+            hash_map: hash_map,
+            start_row: start_row,
+            start_col: start_col,
+            max_steps_to_go: 0,
+        }
+    }
+}
+
+#[derive(Clone)]
+#[derive(Debug)]
+struct Cel {
+    row: i32,
+    col: i32,
+    cost: i32,
+}
+
+impl Ord for Cell {
+    // Compare costs directly for max-heap behavior
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.cost.cmp(&other.cost)
+    }
+}
+
+impl PartialOrd for Cell {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_adding_y_vectors() {
+        assert!(true);
+    }
+}
---
+++ rust/day21/src/main.rs
@@ -0,0 +1,5 @@
+
+fn main() {
+    //let _ = day21::run_part_1(String::from("data/input.txt"));    
+    let _ = day21::run_part_2(String::from("data/input.txt"));    
+}
