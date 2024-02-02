b9313bc9b3a7321e9e788794b5a0ff336ec0805b
oli <oliver.wenger@kemaro.ch>
2024-02-02 15:52:18 +0100

    implemented dijkstra

--- rust/day23/src/lib.rs
+++ rust/day23/src/lib.rs
@@ -7,21 +7,77 @@ pub fn run_part_1(input_path: String) -> Result<(), Box<dyn Error>> {
     let input = fs::read_to_string(input_path)?;
     let grid = parse_input(input);
     let res = dijkstra(&grid);
+    println!("Res: {res}");
     Ok(())
 }
 
-fn dijkstra(grid: &Vec<Vec<char>>) -> i32 {
+fn dijkstra(grid: &Grid) -> u32 {
     let mut visited: Vec<Node> = Vec::new();
     let mut heap: BinaryHeap<Node> = BinaryHeap::new();
-    let start_col = get_start_col(grid);
-    let start_node = Node{ cost: 0, coord: (0, start_col), history: Vec::new()};
+    let start_col = get_start_col(&grid.grid);
+    let start_node = Node{ cost: 0, coord: (0, start_col), history: vec![(0, start_col)]};
     heap.push(start_node);
     
     loop {
         if heap.len() == 0 {
             break;
         }
-        let cur_node = heap.pop();
+        let cur_node = heap.pop().unwrap();
+        println!("{:?}", cur_node);
+
+        if cur_node.coord.0 == grid.rows - 1 {
+            return cur_node.cost;
+        }
+
+        if grid.can_go(&cur_node.coord, Dir::Right) {
+            let new_coord = (cur_node.coord.0, cur_node.coord.1 + 1);
+            if !cur_node.history.contains(&new_coord) {
+                let mut new_history = cur_node.history.clone();
+                new_history.push(new_coord);
+                heap.push(Node{
+                    cost: cur_node.cost + 1,
+                    coord: new_coord,
+                    history: new_history,
+                })
+            }
+        }
+        if grid.can_go(&cur_node.coord, Dir::Down) {
+            let new_coord = (cur_node.coord.0 + 1, cur_node.coord.1);
+            if !cur_node.history.contains(&new_coord) {
+                let mut new_history = cur_node.history.clone();
+                new_history.push(new_coord);
+                heap.push(Node{
+                    cost: cur_node.cost + 1,
+                    coord: new_coord,
+                    history: new_history,
+                })
+            }
+        } 
+        if grid.can_go(&cur_node.coord, Dir::Left) {
+            let new_coord = (cur_node.coord.0, cur_node.coord.1 - 1);
+            if !cur_node.history.contains(&new_coord) {
+                let mut new_history = cur_node.history.clone();
+                new_history.push(new_coord);
+                heap.push(Node{
+                    cost: cur_node.cost + 1,
+                    coord: new_coord,
+                    history: new_history,
+                })
+            }
+        }
+        if grid.can_go(&cur_node.coord, Dir::Up) {
+            let new_coord = (cur_node.coord.0 - 1, cur_node.coord.1);
+            if !cur_node.history.contains(&new_coord) {
+                let mut new_history = cur_node.history.clone();
+                new_history.push(new_coord);
+                heap.push(Node{
+                    cost: cur_node.cost + 1,
+                    coord: new_coord,
+                    history: new_history,
+                })
+            }
+        }
+
     }
     0
 }
@@ -36,8 +92,8 @@ fn get_start_col(grid: &Vec<Vec<char>>) -> usize {
     0
 }
 
-fn parse_input(input: String) -> Vec<Vec<char>> {
-    input.lines().map(|line| line.chars().collect()).collect()
+fn parse_input(input: String) -> Grid {
+    Grid::from_grid(input.lines().map(|line| line.chars().collect()).collect())
 }
 
 #[derive(Debug)]
@@ -58,6 +114,75 @@ impl Ord for Node {
     fn cmp(&self, other: &Self) -> Ordering {
         self.cost.cmp(&other.cost)
     }
+}
+
+struct Grid {
+    grid: Vec<Vec<char>>,
+    rows: usize,
+    cols: usize,
+}
+
+impl Grid {
+    pub fn from_grid(grid: Vec<Vec<char>>) -> Grid {
+        Grid{
+            rows: grid.len(),
+            cols: grid.first().map_or(0, Vec::len),
+            grid: grid,
+        }
+    }
+
+    pub fn can_go(&self, coord: &(usize, usize), dir: Dir) -> bool {
+        match dir {
+            Dir::Right => {
+                if coord.1 >= self.cols - 1 {
+                    return false;
+                }
+                match self.grid[coord.0][coord.1 + 1] {
+                    '.' => return true,
+                    '>' => return true,
+                    _ => return false,
+                }
+            }
+            Dir::Down => {
+                if coord.0 >= self.rows - 1 {
+                    return false;
+                }
+                match self.grid[coord.0 + 1][coord.1] {
+                    '.' => return true,
+                    'v' => return true,
+                    _ => return false,
+                }
+            }
+            Dir::Left => {
+                if coord.1 == 0 {
+                    return false;
+                }
+                match self.grid[coord.0][coord.1 - 1] {
+                    '.' => return true,
+                    '<' => return true,
+                    _ => return false,
+                }
+            }
+            Dir::Up => {
+                if coord.0 == 0 {
+                    return false;
+                }
+                match self.grid[coord.0 - 1][coord.1] {
+                    '.' => return true,
+                    '^' => return true,
+                    _ => return false,
+                }
+            }
+        }
+    }
+
+}
+
+enum Dir {
+    Right,
+    Down,
+    Left,
+    Up
 }
 
 
