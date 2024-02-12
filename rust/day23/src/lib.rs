25d9e51160cea1e8e16dd63b135cfcd8eeaf0e9f
Oliver <owenger@ethz.ch>
2024-02-12 17:06:27 +0100

    added check at edge of graph

--- rust/day23/src/lib.rs
+++ rust/day23/src/lib.rs
@@ -37,33 +37,26 @@ fn dijkstrav2(net: &HashMap<Coord, Vec<(Coord, u32)>>, start_coord: Coord, end_r
     node_vec.push(start_node);
     let mut res_cost: u32 = 0;
     let mut biggest_history: Vec<Coord> = Vec::new();
-    let mut max_dist: usize = 0;
-    let mut max_len: usize = 0;
     let mut count: u32 = 0;
 
     loop {
         if node_vec.len() == 0 {
             break;
         }
-        // if node_vec.len() > max_len {
-        //     max_len = node_vec.len();
-        //     println!("{max_len}");
-        // }
         count += 1;
         if count % 1000000 == 0 {
             println!("Count: {count}");
         }
         let cur_node = node_vec.pop().expect("Heap is empty");
-        let dist = cur_node.coord.row + cur_node.coord.col;
-        if dist > max_dist {
-            max_dist = dist;
-        }
         if cur_node.coord.row == end_row {
             if cur_node.cost > res_cost {
                 res_cost = cur_node.cost;
                 println!("Res cost: {res_cost}");
                 biggest_history = cur_node.history.clone();
             }
+        }
+        if !net.contains_key(&cur_node.coord) {
+            continue;
         }
         let candidates = net.get(&cur_node.coord).unwrap().clone();
         if candidates.len() == 1 && candidates[0].0 == (Coord{ row: 0, col: 0 }) {
@@ -85,94 +78,6 @@ fn dijkstrav2(net: &HashMap<Coord, Vec<(Coord, u32)>>, start_coord: Coord, end_r
     println!("{:?}", biggest_history);
     res_cost
 }
-
-fn dijkstra(grid: &Grid) -> u32 {
-    let mut visited: Vec<Node> = Vec::new();
-    let mut heap: BinaryHeap<Node> = BinaryHeap::new();
-    let mut costs: Vec<Vec<u32>> = vec![vec![0; grid.cols]; grid.rows];
-    let start_col = grid.get_start_col();
-    let start_node = Node{ cost: 0, coord: Coord{ row: 0, col: start_col }, history: vec![Coord{ row: 0, col: start_col }]};
-    heap.push(start_node);
-    let mut res_cost: u32 = 0;
-    let mut biggest_history: Vec<Coord> = Vec::new();
-    let mut prev_cost = 0;
-    let mut max_dist: usize = 0;
-    
-    loop {
-        if heap.len() == 0 {
-            break;
-        }
-        let cur_node = heap.pop().unwrap();
-        let cur_dist = cur_node.coord.row + cur_node.coord.col;
-        if cur_dist > max_dist {
-            max_dist = cur_dist;
-            println!("Max dist: {max_dist}");
-        }
-        //costs[new_coord.0][new_coord.1] = new_cost;
-        if cur_node.coord.row == grid.rows - 1 {
-            if cur_node.cost > res_cost {
-                res_cost = cur_node.cost;
-                biggest_history = cur_node.history.clone();
-            }
-        }
-
-        if grid.can_go(&cur_node.coord, Dir::Right) {
-            let new_coord = Coord{ row: cur_node.coord.row, col: cur_node.coord.col + 1};
-            let new_cost = cur_node.cost + 1;
-            if !cur_node.history.contains(&new_coord) && new_cost > costs[new_coord.row][new_coord.col] {
-                let mut new_history = cur_node.history.clone();
-                new_history.push(new_coord.clone());
-                heap.push(Node{
-                    cost: cur_node.cost + 1,
-                    coord: new_coord,
-                    history: new_history,
-                });
-            }
-        }
-        if grid.can_go(&cur_node.coord, Dir::Down) {
-            let new_coord = Coord{ row: cur_node.coord.row + 1, col: cur_node.coord.col };
-            let new_cost = cur_node.cost + 1;
-            if !cur_node.history.contains(&new_coord) && new_cost > costs[new_coord.row][new_coord.col] {
-                let mut new_history = cur_node.history.clone();
-                new_history.push(new_coord.clone());
-                heap.push(Node{
-                    cost: cur_node.cost + 1,
-                    coord: new_coord,
-                    history: new_history,
-                });
-            }
-        } 
-        if grid.can_go(&cur_node.coord, Dir::Left) {
-            let new_coord = Coord{ row: cur_node.coord.row, col: cur_node.coord.col - 1 };
-            let new_cost = cur_node.cost + 1;
-            if !cur_node.history.contains(&new_coord) && new_cost > costs[new_coord.row][new_coord.col] {
-                let mut new_history = cur_node.history.clone();
-                new_history.push(new_coord.clone());
-                heap.push(Node{
-                    cost: cur_node.cost + 1,
-                    coord: new_coord,
-                    history: new_history,
-                });
-            }
-        }
-        if grid.can_go(&cur_node.coord, Dir::Up) {
-            let new_coord = Coord{ row: cur_node.coord.row - 1, col: cur_node.coord.col };
-            let new_cost = cur_node.cost + 1;
-            if !cur_node.history.contains(&new_coord) && new_cost > costs[new_coord.row][new_coord.col] {
-                let mut new_history = cur_node.history.clone();
-                new_history.push(new_coord.clone());
-                heap.push(Node{
-                    cost: cur_node.cost + 1,
-                    coord: new_coord,
-                    history: new_history,
-                });
-            }
-        }
-    }
-    //print_history(&biggest_history, grid.rows, grid.cols);
-    res_cost
-}
-
 
 fn parse_input(input: String) -> Grid {
     Grid::from_grid(input.lines().map(|line| line.chars().collect()).collect())
@@ -218,6 +123,13 @@ impl Ord for Node {
     fn cmp(&self, other: &Self) -> Ordering {
         other.cost.cmp(&self.cost)
     }
+}
+
+#[derive(Debug)]
+struct SparseGrid {
+    grid: Vec<Vec<char>>,
+    rows: usize,
+    cols: usize,
 }
 
 #[derive(Debug)]
@@ -297,22 +209,31 @@ impl Grid {
             nodes_checked.push(cur_node.clone());
             let debug = cur_node == Coord{ row: 140, col: 139 };
 
-            if self.can_go(&cur_node, Dir::Right) {
+            let can_go_right = self.can_go(&cur_node, Dir::Right);
+            let can_go_down = self.can_go(&cur_node, Dir::Down);
+            let mut can_go_left = self.can_go(&cur_node, Dir::Left);
+            let mut can_go_up = self.can_go(&cur_node, Dir::Up);
+
+            can_go_left &= can_go_down;
+            can_go_up &= can_go_right;
+
+
+            if can_go_right {
                 let right_coord = Coord{ row: cur_node.row, col: cur_node.col + 1 };
                 let (right_cost, right_end) = self.run_path(&right_coord, Dir::Left, &mut nodes_to_check, &nodes_checked, debug);
                 nodes.entry(cur_node.clone()).or_insert_with(Vec::new).push((right_end, right_cost));
             }
-            if self.can_go(&cur_node, Dir::Down) {
+            if can_go_down {
                 let down_coord = Coord{ row: cur_node.row + 1, col: cur_node.col };
                 let (down_cost, down_end) = self.run_path(&down_coord, Dir::Up, &mut nodes_to_check, &nodes_checked, debug);
                 nodes.entry(cur_node.clone()).or_insert_with(Vec::new).push((down_end, down_cost));
             }
-            if self.can_go(&cur_node, Dir::Left) {
+            if can_go_left {
                 let left_coord = Coord{ row: cur_node.row, col: cur_node.col - 1 };
                 let (left_cost, left_end) = self.run_path(&left_coord, Dir::Right, &mut nodes_to_check, &nodes_checked, debug);
                 nodes.entry(cur_node.clone()).or_insert_with(Vec::new).push((left_end, left_cost));
             }
-            if self.can_go(&cur_node, Dir::Up) {
+            if can_go_up {
                 let up_coord = Coord{ row: cur_node.row - 1, col: cur_node.col };
                 let (up_cost, up_end) = self.run_path(&up_coord, Dir::Down, &mut nodes_to_check, &nodes_checked, debug);
                 nodes.entry(cur_node.clone()).or_insert_with(Vec::new).push((up_end, up_cost));
